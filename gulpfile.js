85582622ab319d065d558f9580b6d239927e45c4
Alex <sergalexbard@gmail.com>
2021-05-25 18:36:25 +0300

    first and second section + adaptive

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ gulp.task('server', function() {
 });
 
 gulp.task('styles', function() {
-  return gulp.src("src/scss/*.+(scss|sass)")
+  return gulp.src("src/scss/**/*.+(scss|sass)")
           .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
           .pipe(rename({
             prefix: "",
@@ -25,11 +25,11 @@ gulp.task('styles', function() {
           .pipe(cleanCSS({compatibility: 'ie8'}))
           .pipe(gulp.dest('src/css'))
           .pipe(browserSync.stream());
-})
+});
 
 gulp.task('watch', function () {
-  gulp.watch("src/scss/*.+(scss|sass)", gulp.parallel('styles'))
+  gulp.watch("src/scss/**/*.+(scss|sass)", gulp.parallel('styles'))
   gulp.watch("src/*.html").on('change', browserSync.reload);
-})
+});
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
